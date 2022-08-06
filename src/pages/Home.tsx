622f22066bf5a929e08a334dceea1877cf94c799
Gustavo Costa <cmtehenz@gmail.com>
2022-08-05 21:10:06 -0300

    finalizado desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTask = tasks.map(task => ({...task}))
+
+    const foundItem = updatedTask.find(item => item.id === id);
+    
+    if(!foundItem)
+      return;
+    
+    foundItem.done = !foundItem.done;
+    setTasks(updatedTask);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(item => item.id !== id ))
   }
 
   return (
